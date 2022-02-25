3af8626fc9a9325783cbfc82396fbeb14464b388
minatorak <hotmail22866@gmail.com>
2022-02-25 17:53:27 +0700

    name market property

--- src/main/kotlin/minatorak/capitalcost/properties/NameMarkets.kt
+++ src/main/kotlin/minatorak/capitalcost/properties/NameMarkets.kt
@@ -2,8 +2,7 @@ package minatorak.capitalcost.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.context.annotation.Configuration
 
 @ConfigurationProperties()
 @ConstructorBinding
-class NameMarkets(val markets: List<String>)
+class NameMarkets(val markets: MutableSet<String>, val mapMarketPair: MutableList<String>)
